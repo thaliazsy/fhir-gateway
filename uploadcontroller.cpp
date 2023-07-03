c93553de6c7097902aa3593ecdc4f835cc36e547
Thalia <thalia_noga@yahoo.com>
2023-07-03 19:54:44 +0800

    modify UploadController: add mkpath, add parameter

--- uploadcontroller.cpp
+++ uploadcontroller.cpp
@@ -1,4 +1,5 @@
 #include "uploadcontroller.h"
+#include <QDir>
 
 UploadController::UploadController(QObject* parent)
     : HttpRequestHandler(parent)
@@ -10,18 +11,25 @@ void UploadController::service(HttpRequest &request, HttpResponse &response){
 
     this->request=&request;
     this->response=&response;
+    QByteArray encounter = request.getParameter("encounter-name");
     QByteArray filename = request.getParameter("file-name");
     QTemporaryFile* tempFile=request.getUploadedFile("image-input");
     if (tempFile)
     {
-        QFile file(rootdir + filename);
+        QString path = rootdir + encounter;
+        QDir dir(path);
+
+        if(!dir.exists()){
+            dir.mkpath(".");
+        }
+        QFile file(path + "/" + filename);
         if(file.open(QIODevice::WriteOnly)){
             while (!tempFile->atEnd() && !tempFile->error())
             {
                 QByteArray buffer=tempFile->read(65536);
                 file.write(buffer);
             }
-            response.write(outpath + filename);
+            response.write(outpath + encounter + "/" + filename);
         }
         else {
                response.write("Upload failed. \"file-name\" not found.", true);
