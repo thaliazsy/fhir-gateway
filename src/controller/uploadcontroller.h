b0076f48f9b7aa4489c7c8b1be5eba5d48439a92
Thalia <thalia_noga@yahoo.com>
2023-08-02 13:59:31 +0800

    update repo gateway url

--- src/controller/uploadcontroller.h
+++ src/controller/uploadcontroller.h
@@ -10,7 +10,7 @@ class UploadController: public HttpRequestHandler
     Q_OBJECT
 public:
     QByteArray rootdir = "/home/hapi2/Documents/getfile/skin-lesion/images/";
-    QByteArray outpath = "http://203.64.84.32:9876/getfile/skin-lesion/images/";
+    QByteArray outpath = "http://repo.tcumi.com:9876/getfile/skin-lesion/images/";
     UploadController(QObject* parent=0);
     HttpRequest* request;
     HttpResponse* response;
